f9872b4ff920b80f2db67b76a0d8e36a29fb7f20
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-15 13:31:07 +0800

    Update Lienol-packages.sh

--- Lienol-packages.sh
+++ Lienol-packages.sh
@@ -11,16 +11,16 @@ git clone https://github.com/jerrykuku/node-request
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
 
 git clone https://github.com/fw876/helloworld
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks package
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config
