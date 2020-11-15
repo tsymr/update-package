b6cdc8381e187139c3e49d7c388d6dbf6dbaa0fb
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-15 11:32:51 +0800

    Create lede-packages.sh

---
+++ lede-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git clone https://github.com/rosywrt/luci-theme-rosy
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
+git clone -b master https://github.com/vernesong/OpenClash
+git clone https://github.com/frainzy1477/luci-app-clash
+git clone https://github.com/tty228/luci-app-serverchan
+git clone -b lede https://github.com/pymumu/luci-app-smartdns
+git clone https://github.com/garypang13/luci-app-eqos
+git clone https://github.com/jerrykuku/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
+svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package
+git clone https://github.com/esirplayground/luci-app-poweroff
+rm -rf ./*/.git
+rm -rf ./*/.svn 
+rm -f .gitattributes .gitignore
+exit 0
