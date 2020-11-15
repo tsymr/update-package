bfa404a52fa0e41c0356ba566efb2738068085c6
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-15 14:27:23 +0800

    Update Lienol-packages.sh

--- Lienol-packages.sh
+++ Lienol-packages.sh
@@ -6,11 +6,10 @@ git clone -b master https://github.com/vernesong/OpenClash
 git clone https://github.com/frainzy1477/luci-app-clash
 git clone https://github.com/tty228/luci-app-serverchan
 git clone -b lede https://github.com/pymumu/luci-app-smartdns
+git clone https://github.com/pymumu/smartdns
 git clone https://github.com/garypang13/luci-app-eqos
 git clone https://github.com/jerrykuku/node-request
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
-git clone https://github.com/pymumu/smartdns
-
 git clone https://github.com/fw876/helloworld
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt
@@ -22,10 +21,8 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
-
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config
-
 git clone https://github.com/esirplayground/luci-app-poweroff
 rm -rf ./*/.git
 rm -rf ./*/.svn 
