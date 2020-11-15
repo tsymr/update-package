a6c8d09b75ac1684332dff62b7bd4efaa5bb470d
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-15 14:28:35 +0800

    Update project-packages.sh

--- project-packages.sh
+++ project-packages.sh
@@ -2,7 +2,6 @@
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config
-
 git clone https://github.com/esirplayground/luci-app-poweroff
 rm -rf ./*/.git
 rm -rf ./*/.svn 
