af6eae59eb80a02d6e586d54636a0ae0df98b65c
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-16 09:58:52 +0800

    Update lede.sh

--- lede.sh
+++ lede.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/fw876/helloworld
+mv -f helloworld/luci-app-ssr-plus .
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-dockerman
@@ -8,6 +9,7 @@ git clone https://github.com/rosywrt/luci-theme-rosy
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 git clone -b master https://github.com/vernesong/OpenClash
+mv -f OpenClash/luci-app-openclash .
 git clone https://github.com/frainzy1477/luci-app-clash
 git clone https://github.com/tty228/luci-app-serverchan
 git clone -b lede https://github.com/pymumu/luci-app-smartdns
