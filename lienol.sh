023103f2ff8b4ff274d20908ee0668f01377708f
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-16 10:48:45 +0800

    Update lienol.sh

--- lienol.sh
+++ lienol.sh
@@ -3,12 +3,15 @@ git clone https://github.com/rosywrt/luci-theme-rosy
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 git clone -b master https://github.com/vernesong/OpenClash
+mv -f OpenClash/luci-app-openclash .
 git clone https://github.com/frainzy1477/luci-app-clash
+git clone https://github.com/siropboy/luci-app-vssr-plus
 git clone https://github.com/tty228/luci-app-serverchan
 git clone https://github.com/garypang13/luci-app-eqos
 git clone https://github.com/jerrykuku/node-request
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
 git clone https://github.com/fw876/helloworld
+mv -f helloworld/luci-app-ssr-plus .
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
