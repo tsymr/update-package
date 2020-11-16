e8694d96c2e92742d67d9b1db7a2f6d63768b121
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-16 10:57:54 +0800

    Update lede.sh

--- lede.sh
+++ lede.sh
@@ -17,6 +17,7 @@ git clone https://github.com/jerrykuku/node-request
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package ./
+rm -rf .svn
 git clone https://github.com/esirplayground/luci-app-poweroff
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config
