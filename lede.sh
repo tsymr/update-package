6eec94ca4d951b20e28f5ffce4325890c83ed4d7
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-16 12:01:56 +0800

    Update lede.sh

--- lede.sh
+++ lede.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 git clone https://github.com/fw876/helloworld
 mv -f helloworld/luci-app-ssr-plus .
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-dockerman
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-lib-docker
 git clone https://github.com/rosywrt/luci-theme-rosy
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
@@ -16,7 +14,8 @@ git clone https://github.com/garypang13/luci-app-eqos
 git clone https://github.com/jerrykuku/node-request
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package ./
+rm -rf .svn
 git clone https://github.com/esirplayground/luci-app-poweroff
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config
@@ -28,6 +27,13 @@ svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-theme-opentopd
 svn co https://github.com/siropboy/sirpdboy-package/trunk/adguardhome
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-adguardhome
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-koolproxyR
+
+svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
+svn co https://github.com/lisaac/luci-lib-docker/trunk/collections/luci-lib-docker
+svn co https://github.com/lisaac/luci-app-diskman/trunk ./luci-app-diskman
+rm -rf parted
+mkdir parted
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O parted/Makefile
 rm -rf ./*/.git
 rm -rf ./*/.svn 
 rm -f .gitattributes .gitignore
