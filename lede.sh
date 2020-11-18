8e2e423c5d4dd6c45a8f2319d46b777e62a8ee86
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-19 02:13:50 +0800

    Update lede.sh

--- lede.sh
+++ lede.sh
@@ -7,10 +7,7 @@ mv -f luci-app-docker lede1
 mv -f luci-lib-docker lede1
 mv -f luci-app-netdata lede1
 mv -f k3screenctrl lede1
-
-
-
-
+rm -rf lede1
 git clone https://github.com/rosywrt/luci-theme-rosy
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
@@ -36,18 +33,16 @@ svn co https://github.com/siropboy/sirpdboy-package/trunk/adguardhome
 svn co https://github.com/V2RaySSR/openwrt-packages/trunk/luci-theme-opentomcat
 git clone https://github.com/rufengsuixing/luci-app-onliner
 git clone https://github.com/koshev-msk/luci-app-modeminfo
-
+svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06 ./luci-theme-argon
 svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-app-argon-config
 git clone https://github.com/esirplayground/luci-app-poweroff
 
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-netdata
 svn co https://github.com/siropboy/sirpdboy-package/trunk/netdata
-
 svn co https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng/trunk/luci-app-dockerman
 svn co https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng/trunk/docker-ce
 svn co https://github.com/xiaoqingfengATGH/HomeLede/trunk/package/lean/luci-app-docker
 svn co https://github.com/xiaoqingfengATGH/HomeLede/trunk/package/lean/luci-lib-docker
-
 git clone https://github.com/fw876/helloworld
 mv -f helloworld/luci-app-ssr-plus .
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-vssr
@@ -57,9 +52,9 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/openwrt-u
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/openwrt-Pcap_DNSProxy
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/dnscrypt-proxy-full
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/openwrt-udp2raw
-
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost
 svn co https://github.com/kenzok8/openwrt-packages/trunk/gost
+
 rm -rf .svn
 rm -rf ./*/.git
 rm -rf ./*/.svn 
