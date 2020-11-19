5bae44ca65d2c37002c166a9a469ce5f3e4f1fef
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-19 18:56:59 +0800

    Update lienol.sh

--- lienol.sh
+++ lienol.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
+
 git clone https://github.com/rosywrt/luci-theme-rosy
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
+svn co https://github.com/garypang13/luci-theme-edge/branches/18.06 ./luci-theme-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
-git clone -b master https://github.com/vernesong/OpenClash
-mv -f OpenClash/luci-app-openclash .
+git clone https://github.com/vernesong/OpenClash.git
+mv -f OpenClash/ ./luci-app-openclash
+rm -rf OpenClash
 git clone https://github.com/tty228/luci-app-serverchan
-git clone https://github.com/garypang13/luci-app-eqos
+svn co https://github.com/littletao08/luci-app-eqos/trunk ./luci-app-eqos
 git clone https://github.com/jerrykuku/node-request
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
-git clone https://github.com/fw876/helloworld
-mv -f helloworld/luci-app-ssr-plus .
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev
+svn co https://github.com/lwz322/luci-app-k3screenctrl/trunk ./luci-app-k3screenctrl
+svn co https://github.com/lwz322/k3screenctrl/trunk ./k3screenctrl
+svn co https://github.com/lwz322/k3screenctrl_build/trunk ./k3screenctrl_build
+svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-koolproxyR
+svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-theme-opentopd
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-clash
+svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-adguardhome
+svn co https://github.com/siropboy/sirpdboy-package/trunk/adguardhome
+svn co https://github.com/V2RaySSR/openwrt-packages/trunk/luci-theme-opentomcat
+git clone https://github.com/koshev-msk/luci-app-modeminfo
+svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06 ./luci-theme-argon
+svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-app-argon-config
+git clone https://github.com/esirplayground/luci-app-poweroff
+svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-netdata
+svn co https://github.com/siropboy/sirpdboy-package/trunk/netdata
+svn co https://github.com/fw876/helloworld/trunk ./luci-app-ssr-plus
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
-git clone https://github.com/lwz322/luci-app-k3screenctrl
-git clone https://github.com/lwz322/k3screenctrl
-git clone https://github.com/lwz322/k3screenctrl_build
-svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-koolproxyR
-svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-theme-opentopd
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-clash
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/kcptun
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost
+svn co https://github.com/kenzok8/openwrt-packages/trunk/gost
+svn co https://github.com/destan19/OpenAppFilter/trunk ./luci-app-oaf
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-vssr
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/lua-maxminddb
-svn co https://github.com/V2RaySSR/openwrt-packages/trunk/luci-theme-opentomcat
-git clone https://github.com/rufengsuixing/luci-app-onliner
-rm -rf ./LICENSE
+
+
+sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
+sed -i 's/"network"/"services"/g' ./luci-app-oaf/luci-app-oaf/luasrc/controller/appfilter.lua
+sed -i 's/page = entry({"admin", "network", "eqos"}, cbi("eqos"), "EQoS")/page = entry({"admin", "services", "eqos"}, cbi("eqos"), "内网控制")/g' ./luci-app-eqos/files/eqos-controller.lua
+
 rm -rf ./*/.git
-rm -rf ./*/.svn 
+rm -rf ./*/.svn
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
 rm -f .gitattributes .gitignore
 exit 0
