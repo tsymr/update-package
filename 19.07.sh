ace4a7b0468c420a548540feb83b99a41e0df75f
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-20 08:09:49 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -28,7 +28,6 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/kcptun
 
-
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost
 svn co https://github.com/kenzok8/openwrt-packages/trunk/gost
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-vssr
