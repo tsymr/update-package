85c1a57b5ca7506a7f3d241d71d753cb35e4641a
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-20 14:06:58 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -52,8 +52,6 @@ mv -f haproxy ./passwall
 
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-vssr
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/lua-maxminddb
-
-svn co https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng/trunk/docker-ce
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-dockerman
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-lib-docker
 
