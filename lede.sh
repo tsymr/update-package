52f683cb6e46a88f7433d1a27630763fc8eb5e02
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-27 22:06:59 +0800

    Update and rename lede-openwrt.sh to lede.sh

---
+++ lede.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+svn co  https://github.com/coolsnowwolf/lede/trunk ./openwrt
+
+rm -rf ./.github
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./*/*/.git
+rm -rf ./*/*/.svn
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
