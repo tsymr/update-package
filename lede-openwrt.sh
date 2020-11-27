9e7d67492e6ceab6deb7d4dcc36f631e7bdae92b
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-27 12:34:28 +0800

    Update lede-openwrt.sh

--- lede-openwrt.sh
+++ lede-openwrt.sh
@@ -3,8 +3,6 @@
 svn co  https://github.com/coolsnowwolf/lede/trunk ./openwrt
 svn co  https://github.com/coolsnowwolf/packages/trunk ./packages
 svn co  https://github.com/coolsnowwolf/luci/trunk ./luci
-svn co  https://git.openwrt.org/feed/routing.git/trunk ./routing
-svn co  https://git.openwrt.org/feed/telephony/trunk ./telephony
 svn co  https://github.com/freifunk/openwrt-packages/trunk ./freifunk
 svn co  https://github.com/fw876/helloworld/trunk ./helloworld
 
