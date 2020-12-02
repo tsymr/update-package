85786177379cae8363f66e25ef658e227fef8e35
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-02 20:25:17 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -57,7 +57,7 @@ svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-advanced
 svn co https://github.com/garypang13/openwrt-packages/trunk/cpulimit-ng
 svn co https://github.com/garypang13/openwrt-packages/trunk/cpulimit
 svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-cpulimit
-svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-filebrowser
+svn co https://github.com/MonwF/luci-app-filebrowser/trunk ./luci-app-filebrowser
 
 
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-autopoweroff
