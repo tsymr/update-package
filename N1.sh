4f70aed8a813c6ef044fda6a7359e31d0bba38f5
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-06 09:03:05 +0800

    Create N1.sh

---
+++ N1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/tuanqing/mknop
+
+
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
