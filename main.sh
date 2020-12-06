8db623805454413cfacfa7f598e10dce1f9396f1
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-06 09:47:41 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+svn co https://github.com/tuanqing/mknop/trunk ./
+
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./.github
+rm -rf ./README.md
+rm -rf LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
