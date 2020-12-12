98993a4da9bdc8ab4ef33b7be76feca987de8348
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-12 16:07:26 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,6 @@
 
 svn co https://github.com/tuanqing/mknop/trunk ./
 
-rm -rf ./*/.git
-rm -rf ./*/.svn
-rm -rf ./.github
-rm -rf ./README.md
-rm -rf LICENSE
+rm -rf .git
 rm -rf .svn
-rm -rf ./*/readme.txt
-rm -f .gitattributes .gitignore
 exit 0
