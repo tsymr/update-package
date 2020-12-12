ac8b2065b5bbbcdd7cbd3e081bed29da0165a426
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-12 18:51:30 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 svn co https://github.com/tuanqing/mknop/trunk ./
 
-rm -rf ./.git
-rm -rf ./.github
+rm -rf ./*/.git
 rm -rf ./*/.svn
+rm -rf ./*/*/.svn
+rm -rf ./*/*/.git
+
 rm -rf ./*/README.md
 rm -rf ./*/LICENSE
 rm -rf .svn
