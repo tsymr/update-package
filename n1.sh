8e09411f70c994b173edaf706e9e953950cc3b86
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-12 15:59:16 +0800

    Update n1.sh

--- n1.sh
+++ n1.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-svn co https://github.com/tuanqing/mknop/trunk ./
+svn co https://github.com/tuanqing/mknop/trunk ./123
 
-rm -rf ./*/.git
-rm -rf ./*/.svn
-rm -rf ./.github
-rm -rf ./README.md
-rm -rf LICENSE
-rm -rf .svn
-rm -rf ./*/readme.txt
-rm -f .gitattributes .gitignore
 exit 0
