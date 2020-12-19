3e6fe2a9ca801a314fd9b089f2ee0a680f82e2dd
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-19 17:56:28 +0800

    Update openwrt-18.06-k5.4.sh

--- openwrt-18.06-k5.4.sh
+++ openwrt-18.06-k5.4.sh
@@ -6,16 +6,10 @@ svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-koolproxyR
 git clone https://github.com/esirplayground/luci-app-poweroff
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-autopoweroff
 svn co https://github.com/tuanqing/install-program/trunk ./install-program
-svn co  https://github.com/xiaorouji/openwrt-passwall/trunk ./
-svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
-svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy
-rm -rf ./trojan
-rm -rf ./chinadns-ng
-rm -rf ./kcptun
-rm -rf ./simple-obfs
-rm -rf ./v2ray-plugin
-rm -rf ./shadowsocksr-libev
-svn co https://github.com/fw876/helloworld/trunk ./luci-app-ssr-plus
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
+
+
 
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
 
