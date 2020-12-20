6c360dc91b772e0ab208bcff227d892bcd2a0936
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-20 19:17:19 +0800

    Update openwrt-18.06-k5.4.sh

--- openwrt-18.06-k5.4.sh
+++ openwrt-18.06-k5.4.sh
@@ -6,7 +6,8 @@ svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-koolproxyR
 git clone https://github.com/esirplayground/luci-app-poweroff
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-autopoweroff
 svn co https://github.com/tuanqing/install-program/trunk ./install-program
-
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy
 
 
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
