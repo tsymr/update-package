14cd33d8e5998bb1fc433d51eafa0632bf8e3e1e
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-22 21:50:15 +0800

    Update usb.sh

--- usb.sh
+++ usb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 svn co https://github.com/281677160/update-package/blob/main/README/usb/10-mount
 
-rm -rf .git
-rm -rf .svn
+rm -rf /.git
+rm -rf /.svn
 rm -f .gitattributes .gitignore
 exit 0
