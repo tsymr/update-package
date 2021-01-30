fda83079a31c0c9d126125435b9aa44730b4ab78
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-31 02:06:11 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -53,6 +53,7 @@ svn co https://github.com/tuanqing/luci-app-mentohust/trunk ./luci-app-mentohust
 svn co https://github.com/msylgj/luci-app-tencentddns/trunk ./luci-app-tencentddns
 svn co https://github.com/jerrykuku/luci-app-ttnode/trunk ./luci-app-ttnode
 svn co https://github.com/zzsj0928/luci-app-serverchand/trunk ./luci-app-serverchand
+svn co https://github.com/281677160/openwrt-package/branches/update ./luci-app-autoupdate
 
 
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
