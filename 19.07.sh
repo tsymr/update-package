2262fb0cea8a08eb3aca7b8d43b3edbf79745c9c
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-02 14:24:13 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -43,10 +43,8 @@ svn co https://github.com/firker/diy-ziyong/trunk/luci-app-wrtbwmon-zhcn
 svn co https://github.com/firker/diy-ziyong/trunk/wrtbwmon
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-advanced
 svn co https://github.com/sirpdboy/luci-theme-opentopd/trunk ./luci-theme-opentopd
-svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./lua-maxminddb
 svn co https://github.com/project-openwrt/openwrt-gowebdav/trunk ./luci-app-gowebdav
 svn co https://github.com/iamaluckyguy/luci-app-smartinfo/trunk ./luci-app-smartinfo
-svn co https://github.com/pymumu/openwrt-smartdns/trunk ./smartdns
 svn co https://github.com/tuanqing/install-program/trunk ./install-program
 svn co https://github.com/tuanqing/openwrt-mentohust/trunk ./openwrt-mentohust
 svn co https://github.com/tuanqing/luci-app-mentohust/trunk ./luci-app-mentohust
@@ -56,6 +54,9 @@ svn co https://github.com/zzsj0928/luci-app-serverchand/trunk ./luci-app-serverc
 svn co https://github.com/281677160/openwrt-package/branches/update ./luci-app-autoupdate
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-socat
 svn co https://github.com/garypang13/luci-app-bypass/trunk ./luci-app-bypass
+svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
+svn co https://github.com/garypang13/openwrt-packages/trunk/tcping
+svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns
 
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
 sed -i 's/"网速控制"/"内网控速"/g' ./luci-app-eqos/files/po/zh-cn/eqos.po
