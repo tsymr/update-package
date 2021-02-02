f2cf3d2b3c799ca584b3d3c4e033ca3aeafdb0a1
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-02 21:53:25 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -54,7 +54,7 @@ svn co https://github.com/zzsj0928/luci-app-serverchand/trunk ./luci-app-serverc
 svn co https://github.com/281677160/openwrt-package/branches/update ./luci-app-autoupdate
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-socat
 svn co https://github.com/jerrykuku/lua-maxminddb/trunk/lua-maxminddb
-svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns
+svn co https://github.com/pymumu/openwrt-smartdns/trunk/smartdns
 
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
 sed -i 's/"网速控制"/"内网控速"/g' ./luci-app-eqos/files/po/zh-cn/eqos.po
