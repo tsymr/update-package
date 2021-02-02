0b640605cf1f732ee8b2c87617ca8ccc6cf1fb60
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-02 21:52:23 +0800

    Update openwrt-18.06-k5.4.sh

--- openwrt-18.06-k5.4.sh
+++ openwrt-18.06-k5.4.sh
@@ -9,11 +9,11 @@ svn co https://github.com/tuanqing/install-program/trunk ./install-program
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy
 svn co https://github.com/pymumu/luci-app-smartdns/branches/lede ./luci-app-smartdns
+svn co https://github.com/pymumu/openwrt-smartdns/trunk/smartdns
 svn co https://github.com/zzsj0928/luci-app-serverchand/trunk ./luci-app-serverchand
 svn co https://github.com/281677160/openwrt-package/branches/update ./luci-app-autoupdate
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-socat
 svn co https://github.com/jerrykuku/lua-maxminddb/trunk/lua-maxminddb
-svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns
 
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
 
