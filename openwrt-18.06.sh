97faeb14d97c109a521ced8d513eb2a181f5f9ae
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-06 21:07:04 +0800

    Update openwrt-18.06.sh

--- openwrt-18.06.sh
+++ openwrt-18.06.sh
@@ -7,7 +7,6 @@ git clone https://github.com/esirplayground/luci-app-poweroff
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-autopoweroff
 svn co https://github.com/tuanqing/install-program/trunk ./install-program
 svn co https://github.com/pymumu/luci-app-smartdns/branches/lede ./luci-app-smartdns
-svn co https://github.com/pymumu/openwrt-smartdns/trunk ./smartdns
 svn co https://github.com/zzsj0928/luci-app-serverchand/trunk ./luci-app-serverchand
 svn co https://github.com/281677160/openwrt-package/branches/update ./luci-app-autoupdate
 
