c7932c6c60a93e23192522be45a39c6f0181396e
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-13 19:02:15 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -57,6 +57,8 @@ svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./lua-maxminddb
 svn co https://github.com/pymumu/openwrt-smartdns/trunk/smartdns
 svn co https://github.com/281677160/openwrt-package/branches/adg/luci-app-adguardhome
 svn co https://github.com/281677160/openwrt-package/branches/adg/adguardhome
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t/luci-app-oscam
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t/oscam
 
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
 sed -i 's/"网速控制"/"内网控速"/g' ./luci-app-eqos/files/po/zh-cn/eqos.po
