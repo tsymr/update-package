e24f0f88e0125b27f1d029ba7f8dbd041a51b389
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-14 15:59:01 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -23,11 +23,8 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/kcptun
 
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk ./openwrt-passwall
+svn co https://github.com/fw876/helloworld/trunk ./helloworld
 
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost
 svn co https://github.com/kenzok8/openwrt-packages/trunk/gost
