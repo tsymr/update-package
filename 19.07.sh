35cd8d503fc0e39c73347003eb74c28674e776f5
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-18 18:11:46 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -22,6 +22,9 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/kcptun
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev
 svn co https://github.com/fw876/helloworld/trunk ./helloworld
 
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost
