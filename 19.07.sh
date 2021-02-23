a342e7e0fce1ce0861e5bb91454b97a88cf5ea7d
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-23 14:20:35 +0800

    Update 19.07.sh

--- 19.07.sh
+++ 19.07.sh
@@ -56,6 +56,7 @@ svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t/luci-app-
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t/oscam
 svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lean/v2ray
+svn co https://github.com/CCnut/feed-netkeeper/branches/LUCI-LUA-UCITRACK ./luci-app-netkeeper-interception
 
 
 curl -fsSL  https://raw.githubusercontent.com/281677160/openwrt-package/usb/argon/footer.htm > ./luci-theme-argon/luasrc/view/themes/argon/footer.htm
