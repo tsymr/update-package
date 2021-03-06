16d59a89c7009bd8248114f5fb6fe06722a51ac5
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-07 01:56:28 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -68,6 +68,11 @@ svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lean/v2ray
 svn co https://github.com/CCnut/feed-netkeeper/branches/LUCI-LUA-UCITRACK ./luci-app-netkeeper-interception
 
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/adguardhome
+svn co https://github.com/Lienol/openwrt-packages/trunk/devel/packr
+
+sed -i 's/"免费开源，功能强大的全网络广告和跟踪程序拦截DNS服务器"/去广告和跟踪程序拦截DNS服务器，网页管理账号和密码均为：admin/g' luci-app-adguardhome/po/zh-cn/AdGuardHome.po
 curl -fsSL  https://raw.githubusercontent.com/281677160/openwrt-package/usb/argon/footer.htm > ./luci-theme-argon/luasrc/view/themes/argon/footer.htm
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' ./luci-app-argon-config/po/zh-cn/argon-config.po
 sed -i 's/"网速控制"/"内网控速"/g' ./luci-app-eqos/files/po/zh-cn/eqos.po
