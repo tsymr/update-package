23aa8de56fd325567378abfe12a8432ea581b175
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-08 21:09:41 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -66,7 +66,9 @@ svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
 svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lean/v2ray
 svn co https://github.com/CCnut/feed-netkeeper/branches/LUCI-LUA-UCITRACK ./luci-app-netkeeper-interception
 
-git clone https://github.com/281677160/AdGuardHome
+svn co https://github.com/281677160/AdGuardHome/trunk/adguardhome
+svn co https://github.com/281677160/AdGuardHome/trunk/luci-app-adguardhome
+svn co https://github.com/281677160/AdGuardHome/trunk/packr
 
 
 
