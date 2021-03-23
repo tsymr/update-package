8ed66e8760480ec79af1a44406412bb0515367db
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-23 13:19:27 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -68,7 +68,7 @@ svn co https://github.com/CCnut/feed-netkeeper/branches/LUCI-LUA-UCITRACK ./luci
 svn co https://github.com/281677160/AdGuardHome/trunk/adguardhome
 svn co https://github.com/281677160/AdGuardHome/trunk/luci-app-adguardhome
 svn co https://github.com/Lienol/openwrt-packages/trunk/devel/packr
-git clone https://github.com/linkease/ddnsto-openwrt
+git clone https://github.com/linkease/ddnsto-openwrt/
 
 
 curl -fsSL  https://raw.githubusercontent.com/281677160/openwrt-package/usb/argon/footer.htm > ./luci-theme-argon/luasrc/view/themes/argon/footer.htm
